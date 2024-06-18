c0c352a1fc2ae260c14c0d44e9ede3ec70ed9018
Terry9022 <karta1020376@gmail.com>
2024-06-17 22:03:51 -0400

    start to create some pages

---
+++ pages/events/event1.js
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+    </div>
+  );
+};
+
+export default Page;
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Event page</h1>
+    </div>
+  );
+};
+
+export default Page;
